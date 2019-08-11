9f5dad262ee675b6968b6963ec4aaff9dc104fe4
Hong Sheng Yang <Hsy30009@gmail.com>
2019-08-11 16:57:03 +0800

    update

---
+++ app/src/main/java/com/oslost/tapit/AiActivity.java
@@ -0,0 +1,14 @@
+package com.oslost.tapit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ai);
+    }
+}
