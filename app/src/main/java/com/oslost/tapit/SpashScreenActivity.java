c6204a8bb58e6a7a2eca8ea6695711c2674959c4
Hong Sheng Yang <Hsy30009@gmail.com>
2019-08-15 05:18:23 +0800

    :rocket: SPASH SCREEN DONE 🌚

--- app/src/main/java/com/oslost/tapit/SpashScreenActivity.java
+++ app/src/main/java/com/oslost/tapit/SpashScreenActivity.java
@@ -5,27 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SpashScreenActivity extends AppCompatActivity {
 
+    /* Splash screen timer */
+    private static int SPLASH_TIME_OUT = 2000;
+    private static final String TAG = SpashScreenActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spash_screen);
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-        startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
 
-        /* Music Portion of the splash screen */
-        MediaPlayer mp = MediaPlayer.create(getBaseContext(), R.raw.introtone);
-        mp.start(); //Starts your sound
+            @Override
+            public void run() {
+                Intent intent = new Intent(SpashScreenActivity.this, MapsActivity.class);
 
-        finish();
+                /* Music Portion of the splash screen */
+                MediaPlayer mp = MediaPlayer.create(getBaseContext(), R.raw.introtone);
+                mp.start(); //Starts your sound
+
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
